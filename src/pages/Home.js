0061fd25741b14144501841f9763d80a9378152b
DanteAlonsoHT <dante1818@hotmail.com>
2021-09-21 23:49:44 -0500

    Create Home component

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+const Home = () => (
+  <div className="home-container">
+    <h2>Welcome to our page!</h2>
+    <p>
+      Mathematics is the science that deals with the logic of shape,
+      quantity and arrangement. Math is all around us, in everything we do.
+      It is the building block for everything in our daily lives,
+      including mobile devices, architecture (ancient and modern),
+      art, money, engineering, and even sports..
+    </p>
+    <p>
+      Since the beginning of recorded history, mathematic discovery has been at the
+      forefront of every civilized society, and in use in even the most primitive
+      of cultures. The needs of math arose based on the wants of society.
+      The more complex a society, the more complex the mathematical needs.
+      Primitive tribes needed little more than the ability to count,
+      but also relied on math to calculate the position of the sun and the physics
+      of hunting.
+    </p>
+  </div>
+);
+
+export default Home;
