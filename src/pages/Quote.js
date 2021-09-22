947b05f0b8e030a8fc44505fefec3accc5d945df
DanteAlonsoHT <dante1818@hotmail.com>
2021-09-21 23:49:59 -0500

    Create Quote component

---
+++ src/pages/Quote.js
@@ -0,0 +1,10 @@
+const Quote = () => (
+  <h2 className="quote-section">
+    Mathematics is not about numbers, equations, computations,
+    or algorithms: It is about understanding.
+    <br />
+    {' '}
+    -William Paul Thurston
+  </h2>
+);
+export default Quote;
